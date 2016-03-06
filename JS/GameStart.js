05f24773e9e0f7b6304645d89adbe3b806e19167
Poseidyn <jasonrc83@gmail.com>
2016-03-06 22:22:35 +0000

    Working on board configuration. 2nd Commit.

--- JS/GameStart.js
+++ JS/GameStart.js
@@ -1,15 +1,45 @@
+// Global Variables
 var score = 0;
+var gameWidth = 15;
+var gameHeight = 32;
 var gmBrdCnvs;
 var gmBrdCtx;
 var nxtPieceCnvs;
+var __MOVEDOWN;
+var __MOVELEFT;
+var __MOVERIGHT;
+var crntX;
+var crntY;
 var nxtPieceCtx;
 var pieceChoice;
-var boardArray = new Array(14); // This will keep track of the board for pieces that have hit the bottom
-for (var i = 0; i < boardArray.length; i++) {
-    boardArray[i] = new Array(34); // This makes it a 2 dimensuional array
+var isRotated = false;
+var boardObject = {
+    'row0' : {
+        'column0' : {
+            'value' : '0',
+            'color' : 'darkgrey',
+            'border' : 'darkgrey'
+        }
+    }
+};
+for (var i = 0; i < gameWidth; i++) {
+    for (var j = 0; j < gameHeight; j++) {
+        boardObject['row'+i]['column'+j]['value'] = 0;
+        boardObject['row'+i]['column'+j]['color'] = darkgrey;
+        boardObject['row'+i]['column'+j]['border'] = darkgrey;
+    }
 }
+/*var boardArray = new Array(gameWidth); // This will keep track of the game pieces.
+for (var i = 0; i < boardArray.length; i++) {
+    boardArray[i] = boardObject; // This makes it a 2 dimensional array for moving up/down and left/right
+    for (var j = 0; j < gameHeight; j++) {
+        boardArray[i][j].value = 0; //0 = empty block, 1 = current block that is moving, 2 = locked block (set piece). If whole row is set to 2, row is removed and score is added.
+        boardArray[i][j].color = darkgrey; //Saves color of block, particularly for locked pieces to show correctly when screen is redrawn.
+        boardArray[i][j].border = darkgrey; //Saves border color of block.
+    }
+}*/
 
-function startGame() {
+function startGame() { //initial setup of game and then starts timer
     gmBrdCnvs = document.getElementById("game_play_board"); //get canvas for playing the game
     nxtPieceCnvs = document.getElementById("show_next_piece"); //get canvas for showing next piece
     nxtPieceCnvs.setAttribute('width', '180'); //set canvas width
@@ -18,9 +48,9 @@ function startGame() {
     gmBrdCnvs.setAttribute('width', '420'); //set game board width
     gmBrdCnvs.setAttribute('height', '666'); // set game board height
     gmBrdCtx = gmBrdCnvs.getContext("2d"); //get next piece canvas context
-    selectNextPiece(gmBrdCtx, gmBrdCnvs.width, gmBrdCnvs.height); // Generates the first piece to be sent to the game
-    startTimer();//Starts the game
+   // selectNextPiece(gmBrdCtx, gmBrdCnvs.width, gmBrdCnvs.height); // Generates the first piece to be sent to the game
     selectNextPiece(nxtPieceCtx, nxtPieceCnvs.width, nxtPieceCnvs.height);// Generates the first piece for the "Next Piece" area
+    startTimer();//Starts the game
 }
 
 function startTimer() {
@@ -39,6 +69,7 @@ function selectNextPiece(CanvasCtx, cWidth, cHeight) {
 
 function createPiece(shape, CanvasCtx, cWidth, cHeight) {
     CanvasCtx.clearRect(0, 0, cWidth, cHeight);
+    isRotated = false;
     if (cHeight > 200) { //Makes sure pieces drop in at top of Game Board, instead of middle of the board.
         cHeight = 100;
     }
@@ -160,25 +191,138 @@ function createPiece(shape, CanvasCtx, cWidth, cHeight) {
 }
 
 function movePiece () {
-    
+    gmBrdCtx.clearRect(0, 0, gmBrdCnvs.width, gmBrdCnvs.height);
+    var squareColor;
+    for (var i = 0; i < gameWidth; i++) {  //Set the whole array to 0
+        for (var j = 0; j < gameHeight; j++) {
+            if (boardArray[i][j].value == 1) { //Checks if the location is current piece, if so, sets the color of the square.
+                squareColor = boardArray[i][j].color; //Sets color for redraw of current pieces.
+            }
+            if (boardArray[i][j].value == 2) { //Checks if the location is a locked piece, if so, sets the color of the square.
+                squareColor = boardArray[i][j].color; //Sets color for redraw of locked pieces.
+            } else {
+                squareColor = darkgrey;
+            }
+        }
+    }
+    switch (pieceChoice) {
+        case 0: //Square
+            break;
+        case 1: //Line, 
+            if (isRotated == true) { //Checks if piece has been rotated so that it moves correctly.
+                
+                
+            } else { //Moves piece in non-rotated position
+                
+            }
+            break;
+        case 2: //Z shape, 
+            if (isRotated == true) { //Checks if piece has been rotated so that it moves correctly.
+                
+                
+            } else { //Moves piece in non-rotated position
+                
+            }
+            break;
+        case 3: //T shape, 
+            if (isRotated == true) { //Checks if piece has been rotated so that it moves correctly.
+                
+                
+            } else { //Moves piece in non-rotated position
+                
+            }
+            break;
+        case 4: //J shape, 
+            if (isRotated == true) { //Checks if piece has been rotated so that it moves correctly.
+                
+                
+            } else { //Moves piece in non-rotated position
+                
+            }
+            break;
+        case 5: //S shape, 
+            if (isRotated == true) { //Checks if piece has been rotated so that it moves correctly.
+                
+                
+            } else { //Moves piece in non-rotated position
+                
+            }
+            break;
+        case 6: //L shape, 
+            if (isRotated == true) { //Checks if piece has been rotated so that it moves correctly.
+                
+                
+            } else { //Moves piece in non-rotated position
+                
+            }
+            break;
 }
 
 function rotatePiece () {
     gmBrdCtx.clearRect(0, 0, gmBrdCnvs.width, gmBrdCnvs.height);
+    var squareColor;
+    for (var i = 0; i < gameWidth; i++) {  //Set the whole array to 0
+        for (var j = 0; j < gameHeight; j++) {
+            if (boardArray[i][j].value == 1) { //Checks if the location is current piece, if so, sets the color of the square.
+                squareColor = boardArray[i][j].color; //Sets color for redraw of current pieces.
+            }
+            if (boardArray[i][j].value == 2) { //Checks if the location is a locked piece, if so, sets the color of the square.
+                squareColor = boardArray[i][j].color; //Sets color for redraw of locked pieces.
+            } else {
+                squareColor = darkgrey;
+            }
+        }
+    }
     switch (pieceChoice) {
-        case 0:
+        case 0: //Square, so nothing needs to happen.
             break;
-        case 1:
+        case 1: //Line, 
+            if (isRotated == true) { //Checks if piece has been rotated already, if so, reverts to standard position.
+                isRotated = false;
+                
+            } else { //If Piece is not rotated, rotates the piece.
+                
+            }
             break;
-        case 2:
+        case 2: //Z shape, 
+            if (isRotated == true) { //Checks if piece has been rotated already, if so, reverts to standard position.
+                isRotated = false;
+                
+            } else { //If Piece is not rotated, rotates the piece.
+                
+            }
             break;
-        case 3:
+        case 3: //T shape, 
+            if (isRotated == true) { //Checks if piece has been rotated already, if so, reverts to standard position.
+                isRotated = false;
+                
+            } else { //If Piece is not rotated, rotates the piece.
+                
+            }
             break;
-        case 4:
+        case 4: //J shape, 
+            if (isRotated == true) { //Checks if piece has been rotated already, if so, reverts to standard position.
+                isRotated = false;
+                
+            } else { //If Piece is not rotated, rotates the piece.
+                
+            }
             break;
-        case 5:
+        case 5: //S shape, 
+            if (isRotated == true) { //Checks if piece has been rotated already, if so, reverts to standard position.
+                isRotated = false;
+                
+            } else { //If Piece is not rotated, rotates the piece.
+                
+            }
             break;
-        case 6:
+        case 6: //L shape, 
+            if (isRotated == true) { //Checks if piece has been rotated already, if so, reverts to standard position.
+                isRotated = false;
+                
+            } else { //If Piece is not rotated, rotates the piece.
+                
+            }
             break;
     }
 }
